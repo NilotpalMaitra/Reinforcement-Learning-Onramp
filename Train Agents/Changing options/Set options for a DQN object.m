5ebd728f3342c371246faa0d892128a6da8406d0
Nilot <128976944+NilotpalMaitra@users.noreply.github.com>
2024-05-27 19:34:58 +0530

    Update Set options for a DQN object.m

--- Train Agents/Changing options/Set options for a DQN object.m
+++ Train Agents/Changing options/Set options for a DQN object.m
@@ -6,3 +6,29 @@ agopts = rlDQNAgentOptions("DiscountFactor",1)
 agopts.CriticOptimizerOptions = criticopts
 agopts.EpsilonGreedyExploration.EpsilonDecay = 0.001
 agent = rlDQNAgent(obsInfo,actInfo,agopts)
+
+
+% diceopts = rlTrainingOptions("MaxEpisodes",2000,"MaxStepsPerEpisode",20, ...
+%     "ScoreAveragingWindowLength",100,"StopTrainingCriteria","AverageReward","StopTrainingValue",-3);
+% info = train(agent,slEnv,diceopts)
+% 
+% visualizeQ(agent)
+function visualizeQ(agent)
+net = getModel(getCritic(agent));
+% Get Q values (one for each action) for each observation
+Q = predict(net,dlarray(1:20,"CB")); % convert targets into a dlarray
+Q = extractdata(Q)';  % Get results back as numeric matrix and flip
+[no,na] = size(Q);
+% Visualize result
+imagesc(Q)
+colorbar
+xticks(1:na)
+yticks(1:no)
+xlabel("Action (choice of die)")
+ylabel("Observation (target value)")
+% Add policy to the plot
+[~,idx] = max(Q,[],2);
+hold on
+plot(idx,1:no,"kx")
+hold off
+end
